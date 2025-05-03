4a72fd9a60a742d9a83b2c3157b6451085a7d940
forgnath <richardjgignac@gmail.com>
2025-05-03 13:50:14 -0400

    I/O log added to REPL loop logic

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC = gcc
 CFLAGS = -Wall -Wextra -pedantic -std=c99 -g
-SRC = src/main.c src/shell_interface.c
+SRC = src/main.c src/shell_interface.c src/log.c
 TARGET = mcpmux
 
 all: $(TARGET)
--- src/main.c
+++ src/main.c
@@ -7,26 +7,36 @@
 #include <stdio.h>
 #include <string.h>
 #include "shell_interface.h"
+#include "log.h"
 
 int main(void) {
+    
     char input[1024];
 
+    init_log("sessions/world_state.txt");
+
     while (1) {
+                
         print_prompt();
 
         if (!read_input(input, sizeof(input))) {
             break;
         }
+        
+        log_input(input);
 
         if (strcmp(input, "/quit") == 0) {
             printf("Goodbye!\n");
             break;
         }
 
-
         printf("You said: %s\n", input);
+        
+        log_output(input); // eventually GPT or parsed output
 
     }
+
+    close_log();
 
     return 0;
 
