224e030e6ee7a4041191b035ff6efddce59f4cac
forgnath <richardjgignac@gmail.com>
2025-05-09 00:09:04 -0400

    Comment crazy

--- src/utils.c
+++ src/utils.c
@@ -2,17 +2,15 @@
 #include <ctype.h>
 #include "utils.h"
 
-// Parse checker for strings to use with command input structure
-// checks with strncmp match between a given char array/command
-// and returns true if no match found giving hand_command in 
-// command.c a means to check for input commands
+// Checks if given string starts with specified prefix
+// Uses strncmp to compare string start with prefix
+// Returns true if match, false otherwise
 int starts_with(const char *str, const char *prefix) {
     return strncmp(str, prefix, strlen(prefix)) == 0;
 }
 
-//takes in a given character array and searches through its length starting
-//at the end to one by one look for a space using the isspace() function
-//from string.h and replace that array position with a disappearing null value
+// Removes trailing whitespace from a string
+// Iterates through string from end replacing any spaces with null terminator
 void strip_whitespace(char *str) {
     size_t len = strlen(str);
     while (len > 0 && isspace((unsigned char)str[len - 1])) {
